a364770e0f1a7dafc7c53e05a7dbe65720c6faf7
Tony <11195157+sergeich5@users.noreply.github.com>
2024-09-10 15:19:48 +0300

    examples

---
+++ examples/rowspan-with-column-key.blade.php
@@ -0,0 +1,42 @@
+<x-sergeich5-table-component
+        :columns="[
+            \Sergeich5\LaravelTable\Builder\Column::make('Дата', 'date')
+                ->toDateFormat('F Y')
+                ->setRowSpanable(true),
+            \Sergeich5\LaravelTable\Builder\Column::make('Имя', 'name'),
+            \Sergeich5\LaravelTable\Builder\Column::make('Прибыль', 'revenue')
+                ->setRowSpanable(true, 'date')
+                ->toIntegerFormat(),
+            \Sergeich5\LaravelTable\Builder\Column::make('Долг', 'debt')
+                ->toIntegerFormat()
+                ->setClasses(fn($value) => $value > 0 ? 'red' : ''),
+        ]"
+        :data="[
+            [
+                'date' => '2024-09-01',
+                'name' => 'Joe',
+                'revenue' => 0,
+                'debt' => 1000,
+            ],
+            [
+                'date' => '2024-09-01',
+                'name' => 'David',
+                'revenue' => 0,
+                'debt' => 0,
+            ],
+            [
+                'date' => '2024-08-01',
+                'name' => 'Joe',
+                'revenue' => 0,
+                'debt' => 1000,
+            ],
+        ]" />
+
+<style>
+    .int {
+        text-align: center;
+    }
+    .red {
+        background: pink;
+    }
+</style>
---
+++ examples/simple-table.blade.php
@@ -0,0 +1,40 @@
+<x-sergeich5-table-component
+        :columns="[
+            \Sergeich5\LaravelTable\Builder\Column::make('Дата', 'date')
+                ->toDateFormat('F Y'),
+            \Sergeich5\LaravelTable\Builder\Column::make('Имя', 'name'),
+            \Sergeich5\LaravelTable\Builder\Column::make('Прибыль', 'revenue')
+                ->toIntegerFormat(),
+            \Sergeich5\LaravelTable\Builder\Column::make('Долг', 'debt')
+                ->toIntegerFormat()
+                ->setClasses(fn($value) => $value > 0 ? 'red' : ''),
+        ]"
+        :data="[
+            [
+                'date' => '2024-09-01',
+                'name' => 'Joe',
+                'revenue' => 0,
+                'debt' => 1000,
+            ],
+            [
+                'date' => '2024-09-01',
+                'name' => 'David',
+                'revenue' => 0,
+                'debt' => 0,
+            ],
+            [
+                'date' => '2024-08-01',
+                'name' => 'Joe',
+                'revenue' => 0,
+                'debt' => 1000,
+            ],
+        ]" />
+
+<style>
+    .int {
+        text-align: center;
+    }
+    .red {
+        background: pink;
+    }
+</style>
